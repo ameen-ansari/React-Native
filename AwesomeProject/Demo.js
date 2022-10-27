9af187e78206198a3c5e2ff8260509f527cf2754
M.Ameen <ameen55668@gmail.com>
2022-10-27 12:05:57 +0500

    finnally Make a component in React Native

---
+++ AwesomeProject/Demo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Demo = () => {
+    return (
+        <View >
+            <Text style={{textAlign:'center'}}>
+                Welcome To My Demo Component
+            </Text>
+            <Text style={{textAlign:'center'}}>
+                Finally Done
+            </Text>
+        </View>
+    )
+}
+export default Demo
